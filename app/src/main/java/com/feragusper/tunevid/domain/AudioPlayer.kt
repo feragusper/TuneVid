0eef70167df92948a35162ad5876232e73211b7f
Fernando Perez <feragusper@gmail.com>
2020-08-19 20:06:05 -0300

    a little improvement in how the wave form is generated.

--- app/src/main/java/com/feragusper/tunevid/domain/AudioPlayer.kt
+++ app/src/main/java/com/feragusper/tunevid/domain/AudioPlayer.kt
@@ -4,7 +4,6 @@ import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
 import android.util.Log
-import kotlin.experimental.and
 import kotlin.math.sin
 
 data class AudioPlayer(
@@ -19,32 +18,25 @@ data class AudioPlayer(
         val sample = DoubleArray(numSamples)
         val frequencyRange = startFrequency - endFrequency
         val numSamplesByFreq = numSamples / frequencyRange
-        val generatedSnd = ByteArray(2 * numSamples)
+        val generatedSnd = ShortArray(2 * numSamples)
 
         var currentFreqOfTone = startFrequency
+        var sinePhase = 0.0
         Log.i("MainActivity", "currentFreqOfTone $currentFreqOfTone")
         for (i in 0 until numSamples) {
-            val angle = 2 * Math.PI * i / (sampleRate / currentFreqOfTone)
-            sample[i] = sin(angle)
-
+            sinePhase += sin(2 * Math.PI * currentFreqOfTone / numSamples)
             if (i % numSamplesByFreq.toInt() == 0) {
                 currentFreqOfTone++
                 Log.i("MainActivity", "currentFreqOfTone $currentFreqOfTone")
             }
+            sample[i] = sinePhase
         }
 
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        var idx = 0
-        for (value in sample) {
-            // scale to maximum amplitude
-            val ampMaxValue = (value * 32767).toShort()
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (ampMaxValue and 0x00ff).toByte()
-            generatedSnd[idx++] = (ampMaxValue.toInt() and 0xff00).ushr(8).toByte()
+        for ((idx, value) in sample.withIndex()) {
+            generatedSnd[idx] = (value * Short.MAX_VALUE).toInt().toShort()
         }
 
-        val audioTrack = AudioTrack(
+        @Suppress("DEPRECATION") val audioTrack = AudioTrack(
             AudioManager.STREAM_MUSIC,
             sampleRate, AudioFormat.CHANNEL_OUT_MONO,
             AudioFormat.ENCODING_PCM_16BIT, numSamples * 2,
